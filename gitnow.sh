c5008a6647abefe98b09b8f38aee0687537eb572
Ubuntu <ubuntu@ip-172-31-24-146.ap-southeast-2.compute.internal>
2023-11-26 18:37:32 +0000

    it is commited

--- gitnow.sh
+++ gitnow.sh
@@ -8,6 +8,6 @@ REPO_NAME="FirstAWSPrac"
 git add .
 git commit -m "it is commited"
 
-git push  https://$USERNAME:$TOKEN@github.com/OmayrHere/FirsAWSPrac main
+git push https://github.com/OmayrHere/FirstAWSPrac.git main
 
 
