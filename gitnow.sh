620a80cd588d2fa284f64cc1c44e51e8d69647b6
Ubuntu <ubuntu@ip-172-31-24-146.ap-southeast-2.compute.internal>
2023-11-26 18:55:23 +0000

    it is commited

--- gitnow.sh
+++ gitnow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERNAME="OmayrHere"
-TOKEN="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"
+TOKEN="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"
 REPO_NAME="FirstAWSPrac"
 
 REMOTE_URL="https://${USERNAME}:${TOKEN}@github.com/${USERNAME}/${REPO_NAME}.git"
