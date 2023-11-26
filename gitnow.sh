65108cfcd379faac65de8f28f29fc6140615759e
Ubuntu <ubuntu@ip-172-31-24-146.ap-southeast-2.compute.internal>
2023-11-26 18:41:20 +0000

    it is commited

--- gitnow.sh
+++ gitnow.sh
@@ -4,10 +4,12 @@ USERNAME="OmayrHere"
 TOKEN="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"
 REPO_NAME="FirstAWSPrac"
 
+REMOTE_URL="https://${USERNAME}:${TOKEN}@github.com/${USERNAME}/${REPO_NAME}.git"
+
 
 git add .
 git commit -m "it is commited"
 
-git push https://github.com/OmayrHere/FirstAWSPrac.git main
+git push "$REMOTE_URL" main
 
 
