822061f69bb984a4e49f83a53703dfcfebfcec9f
Ubuntu <ubuntu@ip-172-31-24-146.ap-southeast-2.compute.internal>
2023-11-26 18:27:09 +0000

    it is commited

--- gitnow.sh
+++ gitnow.sh
@@ -5,10 +5,9 @@ TOKEN="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"
 REPO_NAME="FirstAWSPrac"
 
 
-git remote add origin https://$USERNAME:$TOKEN@github.com/OmayrHere/FirsAWSPrac.git
 git add .
 git commit -m "it is commited"
 
-git push origin main
+git push  https://$USERNAME:$TOKEN@github.com/OmayrHere/FirsAWSPrac.git main
 
 
